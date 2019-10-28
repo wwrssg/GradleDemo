54b4faacef3f33656c1d8e3f6667cef36d2e8fae
wwrssg <48876321+wwrssg@users.noreply.github.com>
2019-10-28 13:51:36 +0800

    第二次修改

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,7 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("xiugai");
         System.out.println("Github修改");
     }
 }
